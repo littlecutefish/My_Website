1336f5aa9c686071d62571f4da6b96bdaf4da88b
jolie <jolie92lillian@gmail.com>
2024-09-18 10:47:16 +0800

    organize folder

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-
-//in case js in turned off
-// $(window).on('load', function () {
-//     $("#header-scroll").removeClass("small")
-// });
-
-// $(window).scroll(function () {
-//      var sc = $(window).scrollTop()
-//     if (sc > 1) {
-//         $("#header-scroll").addClass("small")
-//     } else {
-//         $("#header-scroll").removeClass("small")
-//     }
-// });
-
 //scrollspy
 $(window).on('scroll', function () {
    var sections = $('section')
